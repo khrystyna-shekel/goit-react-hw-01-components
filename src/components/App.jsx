dd25a4b5ab26d780f5f8e8c13084f2941f9af4bb
khrystyna-shekel <christina.shekel@gmail.com>
2023-12-03 01:16:16 +0200

    mark-up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,15 @@
+import { Profile } from './Profile/Profile';
+import user from '../assets/user.json';
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../assets/data.json';
+
+import { FriendList } from './FriendList/FriendList';
+import friends from '../assets/friends.json';
+
+import { TransactionHistory } from './Transaction/TransactionHistory';
+import transactions from '../assets/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +19,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
